4da8f5403d1e4924edae92a75f50aa3358d87db8
Vikrant Rajan <vikrant_rajan@thecdm.ca>
2020-10-19 22:14:50 -0700

    API Error Catch Handling

--- vue-app/src/pages/profile.vue
+++ vue-app/src/pages/profile.vue
@@ -1,26 +1,24 @@
 <template>
   <q-page padding>
     <div class="column justify-center items-center">
-
-      <q-card class="my-card"> 
-        <img :src="character.img" alt="" srcset="" @click="alert = true">
+      <q-card class="my-card">
+        <img :src="character.img" alt="" srcset="" @click="alert = true" />
         <q-card-section>
           <div class="text-h6">{{ character.name }}</div>
           <div class="text-subtitle2">{{ character.nickname }}</div>
         </q-card-section>
         <q-card-section>
-          <q-chip 
-          v-for="occupation in character.occupation"
-          color="teal"
-          :key="occupation"
-          text-color="white"
-          icon="bookmark"
+          <q-chip
+            v-for="occupation in character.occupation"
+            color="teal"
+            :key="occupation"
+            text-color="white"
+            icon="bookmark"
           >
-            {{occupation}}
+            {{ occupation }}
           </q-chip>
         </q-card-section>
       </q-card>
-
     </div>
     <q-dialog v-model="alert">
       <q-card>
@@ -34,7 +32,6 @@
           </q-card-actions>
         </q-card-section>
       </q-card>
-      
     </q-dialog>
   </q-page>
 </template>
@@ -43,36 +40,44 @@
 import { QSpinnerCube } from "quasar";
 export default {
   async created() {
+    try {
+      // let response = await fetch('http://no-such-url');
+      this.$q.loading.show({
+        message: "Loading",
+        spinnerColor: "blue",
+        spinner: QSpinnerCube,
+      });
 
-    this.$q.loading.show({
-      message: 'Loading',
-      spinnerColor: 'blue',
-      spinner: QSpinnerCube
-    })
+      let url = `https://www.breakingbadapi.com/api/characters/${this.$route.params.id}`;
+      if (this.$route.params.id === "random") {
+        url = `https://www.breakingbadapi.com/api/character/${this.$route.params.id}`;
+      }
+      const character = await this.$axios.get(`${url}`);
+      setTimeout(() => {
+        this.$q.loading.hide();
+      }, 400);
+      this.character = character.data[0];
 
-    let url = `https://www.breakingbadapi.com/api/characters/${this.$route.params.id}`;
-    if (this.$route.params.id === "random") {
-      url = `https://www.breakingbadapi.com/api/character/${this.$route.params.id}`;
+      if(this.character.birthday === "" || this.character.birthday === null || this.character.birthday === undefined) {
+          this.character.birthday = 'No Birthdate Information Available!'
+      } 
+    } catch (err) {
+      this.$q.loading.hide();
+      alert(err); // Catch error
+      alert("Please refresh the page")
     }
-    const character = await this.$axios.get(`${url}`);
-    setTimeout(() => {
-
-      this.$q.loading.hide()
-    }, 400)
-    console.log(character.data);
-    this.character = character.data[0];
   },
   data() {
     return {
       character: "",
-      alert: false
+      alert: false,
     };
   },
 };
 </script>
 
 <style lang="scss" scoped>
-  .my-card {
-    width: 20%;
-  }
+.my-card {
+  width: 20%;
+}
 </style>
